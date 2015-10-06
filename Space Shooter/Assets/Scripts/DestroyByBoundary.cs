aca156eb0307e6396aa9edbfaec5d29d64fd8305
Zhaoning Cai <zcai9@my.centennialcollege.ca>
2015-10-06 00:11:34 -0400

    Unfinished Project
    needs more details

---
+++ Space Shooter/Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,15 @@
+﻿/*
+ *Zhaoning Cai
+ *300817368
+ *2D Space Shooter Game
+*/
+using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
---
+++ Space Shooter/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,17 @@
+﻿/*
+ *Zhaoning Cai
+ *300817368
+ *2D Space Shooter Game
+*/
+using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    public float speed = 0;
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.GetComponent<Renderer>().material.mainTextureOffset = new Vector3(Time.time * speed, 0f, 0f);
+	}
+}
